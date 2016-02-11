adb42c21f8a90962aeb4936a4f02845b3939172d
hanssens <github@hanssens.com>
2016-02-11 23:56:30 +0100

    Nicely asks for project type and name

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,37 +4,69 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the wondrous ' + chalk.red('generator-hanssens') + ' generator!'
-    ));
+    constructor: function () {
+        yeoman.generators.Base.apply(this, arguments);
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+        this.argument('applicationType', { type: String, required: false, desc: 'the project type to create' });
+        this.argument('applicationName', { type: String, required: false, desc: 'the name of the application' });
+    },
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+    init: function () {
+        this.log(yosay('Hanssens\' Flavored Generators: \r\nThese go to ' + chalk.red('eleven')));
+        this.templatedata = {};
+    },
 
-      done();
-    }.bind(this));
-  },
+    askForType: function () {
+        
+        // TODO: Validate project type, if provided through a startup argument
+        
+        if (!this.type) {
+            var done = this.async();
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  },
+            var prompts = [{
+                type: 'list',
+                name: 'type',
+                message: 'What type of project would you like to create?',
+                choices: [
+                    {
+                        name: 'Angular 2 + Typescript',
+                        value: 'ng2ts'
+                    }, {
+                        name: 'Surprise me!',
+                        value: 'surprise'
+                    }
+                ]
+            }];
+            
+            this.prompt(prompts, function (selectedChoice) {
+                this.applicationType = selectedChoice.type;
+                done();
+            }.bind(this));
+        }
+    },
+    
+    askForName: function () {
+        if (!this.applicationName) {
+            var done = this.async();
+            
+            var prompts = [{
+                name: 'applicationName',
+                message: 'What\'s the name of your application?',
+                default: this.applicationType + 'App'
+            }];
 
-  install: function () {
-    this.installDependencies();
-  }
+            this.prompt(prompts, function (props) {
+                this.applicationName = props.applicationName;
+                done();
+            }.bind(this));          
+        }
+
+    },
+
+    install: function () {
+        // doesn't actually *do* anything yet, only spits out config
+        console.log('Project type\t', this.applicationType);
+        console.log('Project name\t', this.applicationName);
+    }
 });
